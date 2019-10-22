b1e463dcf1770bdbe4abfb70c5a383ddd224db1f
88380368 <1067470237@qq.com>
2019-10-23 00:42:35 +0800

    优化-jiangzhiwei

---
+++ src/main/java/com/goodpower/pvams/model/StationMemberImg.java
@@ -0,0 +1,68 @@
+package com.goodpower.pvams.model;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class StationMemberImg implements Serializable {
+    private Long id;
+
+    private Long stationId;
+
+    private Long memberId;
+
+    private String imgUrl;
+
+    private Date createDttm;
+
+    private Date updateDttm;
+
+    private static final long serialVersionUID = 1L;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getStationId() {
+        return stationId;
+    }
+
+    public void setStationId(Long stationId) {
+        this.stationId = stationId;
+    }
+
+    public Long getMemberId() {
+        return memberId;
+    }
+
+    public void setMemberId(Long memberId) {
+        this.memberId = memberId;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl == null ? null : imgUrl.trim();
+    }
+
+    public Date getCreateDttm() {
+        return createDttm;
+    }
+
+    public void setCreateDttm(Date createDttm) {
+        this.createDttm = createDttm;
+    }
+
+    public Date getUpdateDttm() {
+        return updateDttm;
+    }
+
+    public void setUpdateDttm(Date updateDttm) {
+        this.updateDttm = updateDttm;
+    }
+}
--- src/main/java/com/goodpower/pvams/service/MemberService.java
+++ src/main/java/com/goodpower/pvams/service/MemberService.java
@@ -1,7 +1,10 @@
 package com.goodpower.pvams.service;
 
 import com.goodpower.pvams.mapper.PowerStationMemberMapper;
+import com.goodpower.pvams.mapper.StationMemberImgMapper;
 import com.goodpower.pvams.model.PowerStationMember;
+import com.goodpower.pvams.model.StationMemberImg;
+import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,8 +19,22 @@ public class MemberService {
     @Autowired
     PowerStationMemberMapper memberMapper;
 
+    @Autowired
+    StationMemberImgMapper stationMemberImg;
+
     public void addMember(PowerStationMember member){
         memberMapper.insert(member);
+        if(member.getFileList() != null && !member.getFileList().isEmpty()){
+            for(String url : member.getFileList()){
+                Long stationId = member.getStationId();
+                Long memberId = member.getMemberId();
+                StationMemberImg memberImg = new StationMemberImg();
+                memberImg.setImgUrl(url);
+                memberImg.setMemberId(memberId);
+                memberImg.setStationId(stationId);
+                stationMemberImg.insert(memberImg);
+            }
+        }
     }
 
     public void updateMember(PowerStationMember member){
@@ -38,8 +55,24 @@ public class MemberService {
             param.put("limit",pageSize);
         }
         param.put("stationId",stationId);
-        return memberMapper.selectByField(param);
+        List<PowerStationMember> members = memberMapper.selectByField(param);
+        for(PowerStationMember member : members){
+            Map<String,Object> map = Maps.newHashMap();
+            map.put("stationId",member.getStationId());
+            map.put("memberId",member.getMemberId());
+            List<StationMemberImg> imgs = stationMemberImg.selectByFields(map);
+            List<String> fileList = Lists.newArrayList();
+            List<Map<String,Object>> imgMapList = Lists.newArrayList();
+            for(StationMemberImg memberImg : imgs){
+                fileList.add(memberImg.getImgUrl());
+                Map<String,Object> imgMap = Maps.newHashMap();
+                imgMap.put("name",memberImg.getImgUrl());
+                imgMap.put("url",memberImg.getImgUrl());
+                imgMapList.add(imgMap);
+            }
+            member.setFileList(fileList);
+            member.setImgList(imgMapList);
+        }
+        return members;
     }
-
-
 }
